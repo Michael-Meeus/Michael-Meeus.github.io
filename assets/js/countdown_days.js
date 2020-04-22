9f6f9b43d82f7089282d8f9aaf7c31adab3861b2
Michael-Meeus <Michael.meeus@gmail.com>
2020-04-22 20:00:02 +0200

    More timers :clock:

---
+++ assets/js/countdown_days.js
@@ -0,0 +1,117 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Jun 3, 2020 18:00:00").getTime();
+var startTime = new Date("Feb 22, 2020 20:00:00").getTime();
+
+const FULL_DASH_ARRAY = 283;
+const TIME_LIMIT = countDownDate - startTime;
+const WARNING_THRESHOLD = TIME_LIMIT / 2;
+const ALERT_THRESHOLD = TIME_LIMIT / 4;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+let remainingPathColor = COLOR_CODES.info.color;
+let  timeLeft;
+
+document.getElementById("countdown").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="${TIME_LIMIT}"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label"></span>
+</div>
+`;
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  timeLeft = countDownDate - now;
+
+  document.getElementById("base-timer-label").innerHTML = formatTime(
+    timeLeft
+  );
+  setCircleDasharray();
+  setRemainingPathColor(timeLeft);
+
+  // If the count down is finished, write some text
+  if (timeLeft < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "Kyk mooi na jouself, Suid-Afrika.";
+  }
+}, 1000);
+
+function formatTime(time) {
+  var days = Math.floor(time / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((time % (1000 * 60)) / 1000);
+  //   const minutes = Math.floor(time / 60);
+  //   let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${days}:${hours}:${minutes}:${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+
